8ab67dd15c1797e11039d805842a83493fe625f0
jonniecumming <jonniecumming@gmail.com>
2026-01-07 10:42:43 +0000

    Update project settings and URLs to use 'harmonia_hall_project' for consistency

--- harmonia_hall_project/asgi.py
+++ harmonia_hall_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for harmonia_hall project.
+ASGI config for harmonia_hall_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harmonia_hall.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harmonia_hall_project.settings')
 
 application = get_asgi_application()
--- harmonia_hall_project/urls.py
+++ harmonia_hall_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for harmonia_hall project.
+URL configuration for harmonia_hall_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
