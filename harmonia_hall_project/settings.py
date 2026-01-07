6efeaf71733b462c5287ea8fe389fb156c72a151
jonniecumming <jonniecumming@gmail.com>
2026-01-07 15:04:01 +0000

    Add events app with initial structure and settings with new secure SECRET_KEY in env.py

--- harmonia_hall_project/settings.py
+++ harmonia_hall_project/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
 ]
 
 MIDDLEWARE = [
