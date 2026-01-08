95db71eb2f74a46736ec80bd78d5a63b3c4b3896
jonniecumming <jonniecumming@gmail.com>
2026-01-08 09:53:49 +0000

    Refactor URL patterns in events/urls.py to directly import home_page_view for clarity

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import home_page_view
 
 urlpatterns = [
-    path('', views.home_page_view, name='home'),
+    path('', home_page_view),
 ]
