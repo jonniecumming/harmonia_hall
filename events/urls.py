6d42551ba568099026e7a473ac487e2f6a09abb1
jonniecumming <jonniecumming@gmail.com>
2026-01-12 16:19:29 +0000

    Add event templates and update views, URLs, and settings
    - Create index.html template for events
    - Define HomeView to render index.html with pagination
    - Update URL patterns for events
    - Add static files directory to settings
    - Update base.html to include CSS and improve structure
    - Initialise style.css for custom styles

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import EventListView
 
+# URL patterns for events app
 urlpatterns = [
     path('events/', EventListView.as_view(), name='event-list'),
 ]
--- events/views.py
+++ events/views.py
@@ -4,7 +4,13 @@ from django.views.generic import ListView
 from .models import Event
 # Create your views here.
 
+# home view
+class HomeView(ListView):
+    model = Event
+    template_name = "events/index.html"
+    paginate_by = 6
 
+# list view for events
 class EventListView(ListView):
     model = Event
     template_name = "events/event_list.html"
