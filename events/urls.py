4f06ea34d052d53c9edd80e93b76dd176138a702
jonniecumming <jonniecumming@gmail.com>
2026-01-13 13:54:25 +0000

    Add home view URL pattern and ensure bookings view context object name is set

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from .views import EventListView, BookingsView
 
 # URL patterns for events app
 urlpatterns = [
+    path('', EventListView.as_view(), name='home'),
     path('events/', EventListView.as_view(), name='event-list'),
     path('bookings/', BookingsView.as_view(), name='bookings'),
 ]
