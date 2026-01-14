22ac0b63af6562ba114da30f12d6c33cba0314f7
jonniecumming <jonniecumming@gmail.com>
2026-01-14 15:33:14 +0000

    Add BookingCreateView for user booking functionality with capacity checking

--- events/views.py
+++ events/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 
 from .models import Event, Booking
+from .forms import BookingForm
 # Create your views here.
 
 
@@ -38,3 +41,40 @@ class BookingsView(ListView):
     model = Booking
     template_name = "events/bookings.html"
     context_object_name = "bookings"
+
+
+# create booking view
+class BookingCreateView(LoginRequiredMixin, CreateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = "events/booking_form.html"
+    success_url = reverse_lazy('bookings')
+
+    def get_object(self):
+        """Get the event from the URL slug"""
+        return Event.objects.get(slug=self.kwargs['slug'])
+
+    def form_valid(self, form):
+        """Check capacity before saving booking"""
+        event = self.get_object()
+        tickets_requested = form.cleaned_data['number_of_tickets']
+
+        # Count already booked tickets for this event
+        booked = Booking.objects.filter(event=event).aggregate(
+            total=Sum('number_of_tickets')
+        )['total'] or 0
+
+        # Check if tickets available
+        if booked + tickets_requested > event.capacity:
+            form.add_error('number_of_tickets', 'Not enough tickets available. Only {} tickets left.'.format(
+                    event.capacity - booked
+                ))
+            return self.form_invalid(form)
+
+        # Save booking with user and event
+        booking = form.save(commit=False)
+        booking.user = self.request.user
+        booking.event = event
+        booking.save()
+
+        return super().form_valid(form)
