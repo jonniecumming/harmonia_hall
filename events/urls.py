56fb0fbe627c0545bc5f4e4b3083745ec128d7ac
jonniecumming <jonniecumming@gmail.com>
2026-01-14 17:44:45 +0000

    Fix URL patterns in events app for consistency and clarity

--- events/urls.py
+++ events/urls.py
@@ -5,9 +5,9 @@ from .views import BookingCreateView, BookingsView, EventDetailView, EventListVi
 # URL patterns for events app
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('events/', EventListView.as_view(), name='event-list'),
     path('bookings/', BookingsView.as_view(), name='bookings'),
-    path('whats-on/', WhatsOnView.as_view(), name='whats-on'),
+    path('events/', EventListView.as_view(), name='event-list'),
     path('events/<slug:slug>/', EventDetailView.as_view(), name='event-detail'),
-    path('event/<slug:slug>/book/', BookingCreateView.as_view(), name='book-event')
+    path('events/<slug:slug>/book/', BookingCreateView.as_view(), name='book-event'),
+    path('whats-on/', WhatsOnView.as_view(), name='whats-on'),
 ]
