51c58f6b8a28ed9c8c007ddcc010deaccdbe424c
jonniecumming <jonniecumming@gmail.com>
2026-01-14 12:57:48 +0000

    Refactor views.py to improve code readability by adding spacing between class definitions

--- events/views.py
+++ events/views.py
@@ -4,11 +4,13 @@ from django.views.generic import ListView
 from .models import Event, Booking
 # Create your views here.
 
+
 # home view
 class HomeView(ListView):
     model = Event
     template_name = "events/index.html"
     paginate_by = 6
+
 
 # list view for events
 class EventListView(ListView):
@@ -16,11 +18,13 @@ class EventListView(ListView):
     template_name = "events/event_list.html"
     context_object_name = "events"
 
+
 # list view for what's on
 class WhatsOnView(ListView):
     model = Event
     template_name = "events/whats_on.html"
     context_object_name = "whats_on"
+
 
 # bookings view
 class BookingsView(ListView):
