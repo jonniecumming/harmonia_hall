85aa8af16d618d8099002c000dd8595aa1f72818
jonniecumming <jonniecumming@gmail.com>
2026-01-15 10:41:17 +0000

    Create Booking Update view for logged in users in views.py

--- events/views.py
+++ events/views.py
@@ -41,17 +41,6 @@ class EventDetailView(DetailView):
         return context
 
 
-# bookings view
-class BookingsView(LoginRequiredMixin, ListView):
-    model = Booking
-    template_name = "events/bookings.html"
-    context_object_name = "bookings"
-
-    def get_queryset(self):
-        """Return bookings for the logged-in user"""
-        return Booking.objects.filter(user=self.request.user)
-
-
 # create booking view
 class BookingCreateView(LoginRequiredMixin, CreateView):
     model = Booking
@@ -71,7 +60,7 @@ class BookingCreateView(LoginRequiredMixin, CreateView):
             total=Sum('number_of_tickets')
         )['total'] or 0
         available_seats = event.capacity - booked
-        
+
         context['object'] = event
         context['available_seats'] = available_seats
         return context
@@ -98,5 +87,48 @@ class BookingCreateView(LoginRequiredMixin, CreateView):
         booking.user = self.request.user
         booking.event = event
         booking.save()
+
+        return super().form_valid(form)
+
+
+# bookings view
+class BookingsView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = "events/bookings.html"
+    context_object_name = "bookings"
+
+    def get_queryset(self):
+        """Return bookings for the logged-in user"""
+        return Booking.objects.filter(user=self.request.user)
+
+
+# update booking view
+class BookingUpdateView(LoginRequiredMixin, DetailView):
+    model = Booking
+    form_class = BookingForm
+    template_name = "events/booking_form.html"
+    success_url = reverse_lazy('bookings')
+
+    def get_object(self):
+        """Only allow users to edit their own bookings"""
+        return Booking.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        """Validate capacity before saving"""
+        booking = self.get_object()
+        event = booking.event
+        new_tickets = form.cleaned_data['number_of_tickets']
+
+        # Get bookings for this event EXCLUDING this booking
+        booked = Booking.objects.filter(event=event).exclude(
+            id=booking.id
+        ).aggregate(total=Sum('number_of_tickets'))['total'] or 0
+
+        # Check if new total exceeds capacity
+        if booked + new_tickets > event.capacity:
+            form.add_error('number_of_tickets', 'Not enough tickets available. Only {} tickets left.'.format(
+                    event.capacity - booked
+                ))
+            return self.form_invalid(form)
 
         return super().form_valid(form)
